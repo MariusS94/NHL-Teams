c32c9995f57db334688ab0b9a07d7388205d4fae
MariusS94 <marius1spomer@gmail.com>
2020-08-16 23:25:14 +0200

    Add loadingscreen

--- src/App.js
+++ src/App.js
@@ -7,18 +7,28 @@ import ListItem from "./components/ListItem";
 import ListItemIcon from "./components/ListItemIcon";
 import player from "./assets/eishockey.svg";
 import { fetchData } from "./api/nhl-api";
+import LoadingScreen from "./components/LoadingScreen";
+import hockeypuck from "./assets/puck.svg";
 
+function waitFor(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
 function App() {
   const [team, setTeam] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     async function fetchTeams() {
+      await waitFor(2000);
       const teamInfo = await fetchData();
+      setIsLoaded(true);
       setTeam(teamInfo);
     }
     fetchTeams();
   }, []);
-
+  if (!isLoaded) {
+    return <LoadingScreen src={hockeypuck} alt="loading icon puck" />;
+  }
   return (
     <div className="app">
       <header>NHL-Teams</header>
