61155051dc79c4540beeaabe059e503963914046
MariusS94 <marius1spomer@gmail.com>
2020-08-23 21:59:29 +0200

    Clean code from comments

--- src/pages/AllTeams.js
+++ src/pages/AllTeams.js
@@ -9,28 +9,7 @@ import ListItemIcon from "../components/ListItemIcon";
 import player from "../assets/eishockey.svg";
 import { useQuery } from "react-query";
 
-/* function waitFor(time) {
-  return new Promise((resolve) => setTimeout(resolve, time));
-}
- */
 const AllTeams = () => {
-  /* const [isLoading, setIsLoading] = useState(false);
-  
-  
-  useEffect(() => {
-    async function fetchTeams() {
-      setIsLoading(true);
-      const teamInfo = await fetchData();
-      setTeam(teamInfo);
-      setIsLoading(false);
-    }
-    fetchTeams();
-  }, []);
-  
-  if (isLoading || team === null) {
-    return <LoadingScreen src={hockeypuck} alt="loading icon puck" />;
-  }  */
-  /* const [team, setTeam] = useState(null); */
   const [query, setQuery] = useState("");
   const { isLoading, isError, data, error } = useQuery("teams", fetchData);
 
